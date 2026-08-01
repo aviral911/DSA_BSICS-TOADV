d643896f9083cf5cdfc1ca2e14940904d083a044
aviral911 <aviralpandey8604@gmail.com>
2026-08-01 13:26:16 +0530

    addition of floating values

---
+++ basic_addition.java
@@ -0,0 +1,17 @@
+import java.util.*;
+// scanner use and basic sum and use of floating value
+public class basic_addition {
+      public static void main(String[] args) {
+
+        // scanner use;
+         System.out.println("hy buddy");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("first number ");
+        float n = sc.nextFloat();
+        System.out.println("second number ");
+        float i = sc.nextFloat();
+        float sum = n +i;
+        System.out.println("the sum is :"+sum);
+        sc.close();
+      }
+}
