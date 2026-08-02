0a433d4f3db0591ba6dfdfc2fab82f75151165e7
aviral911 <aviralpandey8604@gmail.com>
2026-08-02 10:50:44 +0530

    counting of digits int a number

---
+++ counting.java
@@ -0,0 +1,17 @@
+//Count the number of digits in a number.
+import java.util.Scanner;
+
+public class counting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cn;
+        int count=0;
+        while(n>0){
+         n = n/10;
+       count ++;
+        }
+        System.out.println(count);
+        sc.close();
+    }
+}
