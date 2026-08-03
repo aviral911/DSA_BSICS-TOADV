7e85b16add12b45032e61fa7eb92d463998b3a1b
aviral911 <aviralpandey8604@gmail.com>
2026-08-03 22:40:02 +0530

    reverse a number

---
+++ reverse.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class reverse {
+    //rev a number
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int rev  = 0;
+        while (n>0) {
+            int dig = n%10;
+            rev = rev*10+dig;
+            n/=10;
+        }
+System.out.println(rev);
+sc.close();
+    }
+}
+
+
