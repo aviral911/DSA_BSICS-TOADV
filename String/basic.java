463797780030826f08ebb2bdf044fb5e28e680f6
aviral911 <aviralpandey8604@gmail.com>
2026-08-10 10:50:30 +0530

    string basic

---
+++ String/basic.java
@@ -0,0 +1,15 @@
+package String;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class basic {
+ public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+     String name = "ram";
+     System.out.println(name);
+     char ar[]= name.toCharArray();
+       System.out.println(Arrays.toString(ar));
+ }
+    
+}
