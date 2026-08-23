4934b513fef950f415afaede5479dcd0b2940ac2
aviral911 <aviralpandey8604@gmail.com>
2026-08-23 23:04:11 +0530

    merged string alternatly

---
+++ twopointer/merge_string_alternatly.java
@@ -0,0 +1,21 @@
+package twopointer;
+
+import java.util.Scanner;
+
+public class merge_string_alternatly {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    String s1 = sc.nextLine();
+ String s2 = sc.nextLine();
+ String result="";
+ int i=0;
+ int j=0;
+ while (i<s1.length()&&j<s2.length()) {
+    result = result + s1.charAt(i);
+     result = result +s2.charAt(j); 
+     i++;
+     j++;   
+ }
+ System.out.print(result);
+    }
+}
--- twopointer/rev.java
+++ twopointer/rev.java
@@ -1,3 +1,4 @@
+package twopointer;
 import java.util.Scanner;
 
 public class rev {
