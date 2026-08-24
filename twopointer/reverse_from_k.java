a868e473280dcd2851bb52ed60f4d168fe0c730b
aviral911 <aviralpandey8604@gmail.com>
2026-08-24 23:51:36 +0530

    reversal fron kth

---
+++ twopointer/reverse_from_k.java
@@ -0,0 +1,34 @@
+package twopointer;
+
+import java.util.Scanner;
+
+public class reverse_from_k {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]= new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]= sc.nextInt();
+
+        }
+        int k=sc.nextInt();
+        int[] temp = new int[n];
+
+        int index = 0;
+        for(int i=k;i<n;i++){
+            temp[index]=arr[i];
+            index++;
+
+
+}   for(int i=0;i<k;i++){
+    temp[index]=arr[i];
+    index++;
+
+
+}
+for(int i=0;i<n;i++){
+    System.out.print(temp[i]+" ");
+}
+ }
+
+}
