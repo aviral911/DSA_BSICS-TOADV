44e0513eed845eb69e368dd2af5e560af0449814
aviral911 <aviralpandey8604@gmail.com>
2026-08-30 23:42:25 +0530

    practice for assessment 5

---
+++ mixproblems/reversestring.java
@@ -0,0 +1,14 @@
+package mixproblems;
+
+import java.util.Scanner;
+
+public class reversestring {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String rev = new StringBuilder(s).reverse().toString();
+        System.out.println(rev);
+
+          
+    }
+}
